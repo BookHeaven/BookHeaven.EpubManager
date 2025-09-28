77747e5ae9e7a1aa06813742e932ce6faeec62af
heasheartfire <gagi1995@gmail.com>
2025-09-28 16:07:03 +0200

    feat(Format): add Format enum and extensions for file type handling

---
+++ Enums/Format.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace BookHeaven.EpubManager.Enums;
+
+public enum Format
+{
+    [Description("EPUB")]
+    Epub = 0,
+    [Description("PDF")]
+    Pdf = 1,
+}
---
+++ Extensions/FormatExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BookHeaven.EpubManager.Enums;
+
+namespace BookHeaven.EpubManager.Extensions;
+
+public static class FormatExtensions
+{
+    public static string GetExtension(this Format format) => format switch
+    {
+        Format.Epub => ".epub",
+        Format.Pdf => ".pdf",
+        _ => throw new ArgumentOutOfRangeException(nameof(format), format, null)
+    };
+    
+    public static string GetName(this Format format)
+    {
+        var type = format.GetType();
+        var memInfo = type.GetMember(format.ToString());
+        if (memInfo.Length <= 0) return format.ToString();
+        var attrs = memInfo[0].GetCustomAttributes(typeof(System.ComponentModel.DescriptionAttribute), false);
+        return attrs.Length > 0 ? ((System.ComponentModel.DescriptionAttribute)attrs[0]).Description : format.ToString();
+    }
+}
