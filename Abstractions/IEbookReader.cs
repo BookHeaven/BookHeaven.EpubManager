663f05a7f9148102ecd5580dd9dc0d12b13b7724
heasheartfire <gagi1995@gmail.com>
2025-09-28 16:09:10 +0200

    feat(IEbookReader): add format agnostic interface for readers

---
+++ Abstractions/IEbookReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using BookHeaven.EpubManager.Entities;
+
+namespace BookHeaven.EpubManager.Abstractions;
+
+public interface IEbookReader : IDisposable
+{
+    Task<Ebook> ReadMetadataAsync(string path);
+    Task<Ebook> ReadAllAsync(string path);
+    Task<string> ApplyHtmlProcessingAsync(string content);
+}
