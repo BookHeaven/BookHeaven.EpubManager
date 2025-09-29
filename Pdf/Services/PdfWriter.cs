5be923602f2e04f8ee6c263f45377c515e135719
heasheartfire <gagi1995@gmail.com>
2025-09-29 20:51:56 +0200

    fix(PdfWriter): fix metadata replacing method

--- Pdf/Services/PdfWriter.cs
+++ Pdf/Services/PdfWriter.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using BookHeaven.EpubManager.Abstractions;
 using BookHeaven.EpubManager.Entities;
@@ -9,36 +10,42 @@ namespace BookHeaven.EpubManager.Pdf.Services;
 
 public class PdfWriter : IEbookWriter
 {
+    private static Task ProcessPdfAsync(string bookPath, Action<PdfDocument, PdfDocument> processAction)
+    {
+        var tempPath = Path.GetTempFileName();
+        using (var srcDoc = new PdfDocument(new iText.Kernel.Pdf.PdfReader(bookPath)))
+        using (var destDoc = new PdfDocument(new iText.Kernel.Pdf.PdfWriter(tempPath)))
+        {
+            processAction(srcDoc, destDoc);
+        }
+        File.Copy(tempPath, bookPath, true);
+        File.Delete(tempPath);
+        return Task.CompletedTask;
+    }
+
     public Task ReplaceMetadataAsync(string bookPath, Ebook ebook)
     {
-        using var pdfDocument = new PdfDocument(new iText.Kernel.Pdf.PdfWriter(bookPath));
-        var info = pdfDocument.GetDocumentInfo();
-        info.SetTitle(ebook.Title);
-        info.SetAuthor(ebook.Author);
-        if (!string.IsNullOrWhiteSpace(ebook.Synopsis))
-            info.SetSubject(ebook.Synopsis);
-        
-        return Task.CompletedTask;
+        return ProcessPdfAsync(bookPath, (srcDoc, destDoc) =>
+        {
+            srcDoc.CopyPagesTo(1, srcDoc.GetNumberOfPages(), destDoc);
+            var info = destDoc.GetDocumentInfo();
+            info.SetTitle(ebook.Title);
+            info.SetAuthor(ebook.Author);
+            if (!string.IsNullOrWhiteSpace(ebook.Synopsis))
+                info.SetSubject(ebook.Synopsis);
+        });
     }
 
     public Task ReplaceCoverAsync(string bookPath, string newCoverPath)
     {
-        var tempPath = Path.GetTempFileName();
-
-        using (var srcDoc = new PdfDocument(new iText.Kernel.Pdf.PdfReader(bookPath)))
-        using (var destDoc = new PdfDocument(new iText.Kernel.Pdf.PdfWriter(tempPath)))
+        return ProcessPdfAsync(bookPath, (srcDoc, destDoc) =>
         {
             var coverImage = ImageDataFactory.Create(newCoverPath);
             var pageSize = srcDoc.GetFirstPage().GetPageSize();
             var coverPage = destDoc.AddNewPage(new iText.Kernel.Geom.PageSize(pageSize));
             var pdfCanvas = new iText.Kernel.Pdf.Canvas.PdfCanvas(coverPage);
             pdfCanvas.AddImageFittedIntoRectangle(coverImage, pageSize, false);
-
             srcDoc.CopyPagesTo(2, srcDoc.GetNumberOfPages(), destDoc);
-        }
-
-        File.Copy(tempPath, bookPath, true);
-        File.Delete(tempPath);
-        return Task.CompletedTask;
+        });
     }
 }
